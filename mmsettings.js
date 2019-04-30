a61b23a3795e039be9c8e923fd7009a90642d068
dani0001414 <dani0001414@gmail.com>
2019-04-30 19:22:24 +0200

    Update mmsettings.js

--- mmsettings.js
+++ mmsettings.js
@@ -179,7 +179,7 @@ function createcookie(name, value, days, banner) {
     */
 }
 function calendarListGet(){
-     gapi.client.calendar.calendarList.list({})
+     return gapi.client.calendar.calendarList.list({})
     .then(function(response) {
             // Handle the results here (response.result has the parsed body).
             console.log("Response", response);
