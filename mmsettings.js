8604def69cc4d88b09f54e98cf6df2409124d726
dani0001414 <dani0001414@gmail.com>
2019-04-30 20:54:15 +0200

    Update mmsettings.js

--- mmsettings.js
+++ mmsettings.js
@@ -87,7 +87,7 @@ function calendarEvents(calID) {
         .then(function (response) {
             // Handle the results here (response.result has the parsed body).
             console.log("Response", response);
-            calendarEventsList=response;
+            calendarEventsList=response.result;
             document.getElementById("content").innerHTML = calendarEventsList.items[0];
 
         },
