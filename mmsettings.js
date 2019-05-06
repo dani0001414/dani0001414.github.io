7dbd621b0c02c8965532d63454e810731ca1757f
dani0001414 <dani0001414@gmail.com>
2019-05-07 01:48:29 +0200

    Update mmsettings.js

--- mmsettings.js
+++ mmsettings.js
@@ -243,4 +243,5 @@ function ListSelectIndex() {
         document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
         document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
     }
+    location.reload();
 }
