39be3ce94b5e987135bc908fa2f324c10247dfe8
dani0001414 <dani0001414@gmail.com>
2019-05-09 01:41:38 +0200

    Update mmsettings.js

--- mmsettings.js
+++ mmsettings.js
@@ -97,7 +97,7 @@ function loadClientGDrive() {
 
 function insertFile() {
     var fileMetadata = {
-        'title': 'settings.json',
+        'title': 'settings',
         'mimeType' : 'application/vnd.google-apps.document',
         'parents': [{ id: 'appDataFolder' }]
     };
